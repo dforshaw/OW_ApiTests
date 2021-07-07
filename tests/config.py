33baaa5b28559d952679eb1f97ac2448775dbcfe
dforshaw <dfcv126@gmail.com>
2021-07-06 23:40:48 -0700

    Added comments

--- tests/config.py
+++ tests/config.py
@@ -1,12 +1,13 @@
 import pytest
 
-
+# All API calls fall under the Base URL
 @pytest.fixture
 def baseurl_value():
     baseurl = 'https://api.openweathermap.org/data/2.5/weather'
     return baseurl
 
 
+# Register for Open Weather API ID at https://openweathermap.org/current
 @pytest.fixture
 def appid_value():
     appid = '<Enter OPEN WEATHER API ID here>'
